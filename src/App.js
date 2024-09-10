87d1989d8f4d1a9b184f8b50519e10fc4ed4d805
Pilon2020 <123617070+Pilon2020@users.noreply.github.com>
2024-09-10 04:47:49 -0700

    Trying to fix the initial page not loading to the correct address

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Racing from './pages/Racing';
